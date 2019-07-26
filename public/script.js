81853874576fdd40155e5693e64381d3dacc61e4
alexigityan <alexigityan@yahoo.com>
2019-07-26 15:17:15 +0400

    implement edit function via ajax

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const deleteButtons = document.querySelectorAll('.delete-button');
+const editButtons = document.querySelectorAll('.edit-button');
+
 
 deleteButtons.forEach(activateDeleteButton);
+editButtons.forEach(activateEditButton);
+
+document.querySelector('#close-editor').addEventListener('click', hideEditor);
 
 function activateDeleteButton(button) {
   const target = button.getAttribute('data-target');
@@ -9,7 +14,46 @@ function activateDeleteButton(button) {
       .then(res => res.json())
       .then(todos => displayTodos(todos))
       .catch(err => console.log(err));
-  })
+  });
+}
+
+function activateEditButton(button) {
+  const currentText = button.getAttribute('data-text');
+  const target = button.getAttribute('data-target');
+  button.addEventListener('click', ()=>{
+    editEntry(target, currentText);
+  });
+}
+
+function showEditor() {
+  const editor = document.querySelector('#editor');
+  editor.style.display = '';
+}
+
+
+function hideEditor() {
+  const editor = document.querySelector('#editor');
+  editor.style.display = 'none';
+}
+
+function editEntry(target, currentText) {
+  showEditor();
+  const editText = document.querySelector('#edit-text');
+  editText.value = currentText;
+  document.querySelector('#edit-button').addEventListener('click', ()=>{
+    const newTodo = editText.value;
+    fetch(`api/todos/${target}`, { 
+      method: 'put', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ newTodo })
+    })
+      .then(res => res.json())
+      .then(todos => {hideEditor(), displayTodos(todos)})
+      .catch(err => console.log(err));
+  });
+
 }
 
 function displayTodos(todos) {
@@ -26,7 +70,9 @@ function displayTodos(todos) {
     const editButton = document.createElement('button');
     editButton.className = 'edit-button';
     editButton.setAttribute('data-target', todo.id);
+    editButton.setAttribute('data-text', todo.text);
     editButton.innerText = 'Edit';
+    activateEditButton(editButton);
     todoRow.appendChild(editButton);
     const deleteButton = document.createElement('button');
     deleteButton.className = 'delete-button';
