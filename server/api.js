0b8069a60a2b11b4cca8bbaee4aa8126ee37d7f8
alexigityan <alexigityan@yahoo.com>
2019-07-26 13:43:42 +0400

    implement adding new todos

--- server/api.js
+++ server/api.js
@@ -4,7 +4,32 @@ const api = express.Router();
 
 api.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
-  ///TODO
+  const { todos } = res.locals;
+
+  if (!id || !todos.find(todo => todo.id.toString() === id)) 
+    res.sendStatus(400);
+
+  todos = todos.filter(todo => todo.id.toString() !== id);
+  res.setHeader(200);
+  res.json(todos);  
+});
+
+api.put('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const { todos } = res.locals;
+  const { newTodo } = req.body;
+
+  if (!id || !newTodo || !todos.find(todo => todo.id.toString() === id)) 
+    res.sendStatus(400);
+
+  todos = todos.map(todo => {
+    if(todo.id.toString() === id)
+      return { id: todo.id, text: newTodo };
+    else
+      return todo;
+  });
+  res.setHeader(200);
+  res.json(todos);  
 });
 
 module.exports = api;
--- server/app.js
+++ server/app.js
@@ -17,21 +17,43 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use( (req, res, next) => {
   const { todos } = app.locals;
   const { uid } = req.cookies;
-  const userId = (uid && todos.uid) ? uid : generateId();
+  const userId = (uid && todos[uid]) ? uid : generateId();
 
   res.cookie('uid', userId, {
     maxAge: 7*24*60*60*1000 // cookie expires in a week
   });
-  todos.uid = [];
-  res.locals.userId = userId;
+  if (!todos[userId]) {
+    todos[userId] = [];
+    console.log(todos[userId]);
+    Array.prototype.addTodo = function(todo) {
+      if (!this.freeId) {
+        this.freeId = 1;
+      }
+      this.push({ id: this.freeId++, text: todo });
+    }
+  }
+  
+  res.locals.todos = todos[userId];
   next();
 });
 
 app.use('/api', api);
 
-app.get('/', (req, res)=>{
-  res.render('index');
-});
+app.route('/')
+  .get((req, res) => {
+    console.log(app.locals.todos);
+    res.render('index');  
+  })
+  .post((req, res) => {
+    const { todos } = res.locals;
+    const { todo } = req.body;
+
+    if (!todo) 
+      res.sendStatus(400);
+
+    todos.addTodo(todo);
+    res.render('index');  
+  });
 
 app.get('/edit', (req, res)=>{
   res.render('edit');
