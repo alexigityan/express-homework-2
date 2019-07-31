a0f9626974195a7fe4c144ca7bc2d5971e4e5408
alexigityan <alexigityan@yahoo.com>
2019-08-01 01:51:57 +0400

    remove pug, add static index.html, move get and post routes to api

--- server/api.js
+++ server/api.js
@@ -3,6 +3,28 @@ const express = require('express');
 const api = express.Router();
 api.use(express.json());
 
+api.get('/todos/', (req, res) => {
+  const { todoList } = res.locals;
+  todoList.getTodos()
+    .then( todos => res.json(todos) )
+    .catch( err => (console.log(err), res.sendStatus(500)));     
+})
+
+api.post('/todos/',(req, res) => {
+  const { todoList } = res.locals;
+  const { todo } = req.body;
+
+  if (!todo) {
+    res.sendStatus(400);
+    return;
+  }
+
+  todoList.add(todo)
+    .then( () => todoList.getTodos())    
+    .then( todos => res.json(todos) )
+    .catch( err => (console.log(err), res.sendStatus(500)));     
+});
+
 api.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
   let { todoList } = res.locals;
--- server/app.js
+++ server/app.js
@@ -20,11 +20,7 @@ const TodoList = require('../classes/TodoList');
 /* Routes */
 const api = require('./api');
 
-
-
 const app = express();
-
-app.set('view engine', 'pug');
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -43,28 +39,6 @@ app.use( (req, res, next) => {
 });
 
 app.use('/api', api);
-
-app.route('/')
-  .get((req, res) => {
-    const { todoList } = res.locals;
-    todoList.getTodos()
-      .then( todos => res.render('index', { todos }) )
-      .catch( err => (console.log(err), res.sendStatus(500)));     
-  })
-  .post((req, res) => {
-    const { todoList } = res.locals;
-    const { todo } = req.body;
-
-    if (!todo) {
-      res.sendStatus(400);
-      return;
-    }
-
-    todoList.add(todo)
-      .then( () => todoList.getTodos())    
-      .then( todos => res.render('index', { todos }) )
-      .catch( err => (console.log(err), res.sendStatus(500)));     
-  });
 
 connectDb()
   .then( () => {
