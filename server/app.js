80d6bd8726c9374813b2b2748f5b130108826cf5
alexigityan <alexigityan@yahoo.com>
2019-08-01 05:27:07 +0400

    remove unused middleware

--- server/app.js
+++ server/app.js
@@ -23,7 +23,6 @@ const api = require('./api');
 const app = express();
 
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use( (req, res, next) => {
